dd9459039b2b82be2285a808e90adf43dbe3b3f4
ps220812 <ps220812@summacollege.nl>
2022-01-20 16:06:09 +0100

    added more migrations/seeders

---
+++ P4Web/database/seeders/orderStatusSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\orderStatus;
+use Illuminate\Database\Seeder;
+
+class OrderStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $orderStatus = [
+            ['id' => 1, 'status' => 'bestelling ontvangen'],
+            ['id' => 2, 'status' => 'in bereiding'],
+            ['id' => 3, 'status' => 'in de oven'],
+            ['id' => 4, 'status' => 'klaar in de winkel'],
+            ['id' => 5, 'status' => 'bestelling onderweg'],
+            ['id' => 6, 'status' => 'bezorgd'],
+        ];
+
+        foreach ($orderStatus as $role) {
+            orderStatus::create($role);
+        }
+    }
+}
