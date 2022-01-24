e19ffc6ece04be0a0fb81696971c607138162671
Timo Hoogervorst <PS206169@summacollege.nl>
2022-01-24 13:24:50 +0100

    extra classes en layout

--- P4WPF/P4WPF/Models/Base.cs
+++ P4WPF/P4WPF/Models/Base.cs
@@ -57,6 +57,92 @@ namespace P4WPF.Models
             }
             return null;
         }
+        //public List<> GetAllOrders()
+        //{
+        //    List<> result = new List<>();
+        //    try
+        //    {
+        //        _connection.Open();
+        //        MySqlCommand sql = _connection.CreateCommand();
+        //        sql.CommandText = "SELECT *  FROM dbo.tblGuest";
+        //        MySqlDataReader reader = sql.ExecuteReader();
+        //        DataTable table = new DataTable();
+        //        table.Load(reader);
+        //        foreach (DataRow row in table.Rows)
+        //        {
+        //             = new ();
+        //             = (int)row[""];
+        //             = (string)row[""];
+        //             = (string)row[""];
+        //            result.Add();
+        //        }
+
+        //    }
+        //    catch (Exception e)
+        //    {
+
+        //        Console.Write(e.Message);
+        //        return null;
+        //    finally
+        //    {
+        //        if (_connection.State == ConnectionState.Open)
+        //        {
+        //            _connection.Close();
+        //        }
+        //    }
+
+        //    return result;
+        //}
+        //public bool SaveKlant()
+        //{
+        //    bool result = true;
+        //    try
+        //    {
+        //        if (_connection.State == ConnectionState.Closed)
+        //        {
+        //            _connection.Open();
+        //        }
+        //        MySqlCommand sql = _connection.CreateCommand();
+        //        sql.CommandText =
+        //            @"INSERT INTO 
+        //                ()
+        //                VALUES
+        //                ();";
+
+
+        //        sql.Parameters.AddWithValue("", );
+        //        sql.Parameters.AddWithValue("", );
+        //        sql.ExecuteNonQuery();
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("******");
+        //        Console.WriteLine(e.Message);
+        //        result = false;
+        //    }
+
+        //    finally
+        //    {
+        //        if (_connection.State == ConnectionState.Open)
+        //        {
+        //            _connection.Close();
+        //        }
+        //    }
+        //    return result;
+        //}
+        //public void DeleteFavCountry()
+        //{
+        //    _connection.Open();
+        //    MySqlCommand cmd = _connection.CreateCommand();
+        //    if ( > 0)
+        //    {
+        //        cmd.CommandText = "DELETE FROM  Where  = ";
+        //    }
+
+        //    cmd.Parameters.AddWithValue("", );
+        //    cmd.ExecuteNonQuery();
+        //    _connection.Close();
+        //}
 
     }
 }
---
+++ P4WPF/P4WPF/Models/Orders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4WPF.Models
+{
+    class Orders
+    {
+        private int status_id;
+
+        public int Status_ID
+        {
+            get { return status_id; }
+            set { status_id = value; }
+        }
+        private int pizza_id;
+
+        public int Pizza_ID
+        {
+            get { return pizza_id; }
+            set { pizza_id = value; }
+        }
+        private int user_id;
+
+        public int User_ID
+        {
+            get { return user_id; }
+            set { user_id = value; }
+        }
+    }
+}
