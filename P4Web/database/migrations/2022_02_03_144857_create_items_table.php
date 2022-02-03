26a57892784e92952f61e60a2898ffae2530c4ab
ps220812 <ps220812@summacollege.nl>
2022-02-03 16:17:10 +0100

    added 'items' to database

---
+++ P4Web/database/migrations/2022_02_03_144857_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('quantity');
+            $table->foreignId('unit')->constrained('units');
+            $table->foreignId('ingredient')->constrained('ingredients');
+            $table->integer('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
---
+++ P4Web/database/seeders/ItemsSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use app\Models\items;
+use Illuminate\Database\Seeder;
+
+class ItemsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $units = [
+            ['id' => 1, 'quantity' => '100', 'unit' => '1', 'ingredient' => '3', 'value' => 80],
+            ['id' => 2, 'quantity' => '10', 'unit' => '2', 'ingredient' => '2', 'value' => 40],
+        ];
+
+        foreach ($units as $unit) {
+            items::create($unit);
+        }
+        //
+    }
+}
