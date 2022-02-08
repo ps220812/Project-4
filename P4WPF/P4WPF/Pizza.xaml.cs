bcb6075c73db9f15d6a64e5bdfe4d64acb96eb2b
Timo Hoogervorst <PS206169@summacollege.nl>
2022-02-08 12:33:04 +0100

    jordy's pizza window for crud

---
+++ P4WPF/P4WPF/Pizza.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P4WPF
+{
+    /// <summary>
+    /// Interaction logic for Pizza.xaml
+    /// </summary>
+    public partial class Pizza : Window
+    {
+        public Pizza()
+        {
+            InitializeComponent();
+        }
+    }
+}
